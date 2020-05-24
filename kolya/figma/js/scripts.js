79350b551e6038a7cadba34853f7ebe373903ab8
Nikolay <nikoguitar1995@gmail.com>
2020-05-25 00:59:27 +0300

    validation and galleryWrapper with ternar

--- kolya/figma/js/scripts.js
+++ kolya/figma/js/scripts.js
@@ -1,1 +1,39 @@
+var form = document.getElementsByTagName('form')[0];
+var text = document.getElementById('textInput');
+var email = document.getElementById('emailInput');
+var error = document.querySelector('.error');
+var errorName = document.querySelector('.errorName');
 
+
+email.addEventListener('input', function(event) {
+    if(email.validity.valid) {
+        error.innerHTML = "";
+        error.className = "error";
+    }
+}, false);
+
+text.addEventListener('input', function(event) {
+    if(text.validity.valid) {
+        errorName.innerHTML = "";
+        errorName.className = "errorName";
+
+    }
+}, false);
+
+form.addEventListener('submit', function(event) {
+    if (!email.validity.valid || email.value == "") {
+        error.innerHTML = "Type your email";
+        error.className = "error active";
+        if(!text.validity.valid || text.value == "") {
+            errorName.innerHTML = "Type your text";
+            errorName.className = "errorName active";
+        }
+        event.preventDefault();
+    }
+    else if (!text.validity.valid || text.value == "") {
+        errorName.innerHTML = "Type your text";
+        errorName.className = "errorName active";
+        event.preventDefault();
+    }
+    
+}, false);
--- kolya/jshome/home2.js
+++ kolya/jshome/home2.js
@@ -1,62 +1,145 @@
-// var user = {
-//     name: 'Vasya',
-//     age: 18,
-//     skill: [],
-//     addSkill: function(skill) {
-//         this.skill.push(skill);
-//     },
-//     getName: function () {
-//         return this.name;
-//     },
-//     setName: function (name) {
-//         this.name = name;
-//     }
-// };
-
-
-var user = {
-    name: 'Vasya',
-    age: 18,
-    skill: [],
-    addSkill: addSkill,
-    getName: function () {
-        return this.name;
-    },
-    setName: function (name) {
-        this.name = name;
-    }
-};
-
-var sotrudniky = {
-    names: [],
-    skill: []
-}
-
-function addSkill(skill) {
-    this.skill.push(skill);
-};
-
-setTimeout(addSkill.bind(sotrudniky, 'js'), 1000)
-addSkill.apply(user, ["css"]);
-addSkill.call(user, 'css');
-addSkill.call(sotrudniky, 'html');
-console.log(user);
-console.log(sotrudniky);
-
-function slider() {
+function slider(getIdWrapper) {
     var collection = [];
-    return  {
-            
-            addCollection: function(item) {
-                console.log(item);
-            },
-            getCollection: function() {
-                return collection;
-            },
-            removeCollection: function(src) {
+    var galleryWrapper = document.getElementById(getIdWrapper);
+    console.log(galleryWrapper);
+    function removeHtml(src) {
+        galleryWrapper.removeChild(galleryWrapper.querySelector('[src="' + src + '"]'));
+    }
     
+    function sortCollection(condition) {
+        collection.sort(function (a, b) {
+            a = (condition === 'src') ? a[condition] : a.attributes[condition];
+            b = (condition === 'src') ? b[condition] : b.attributes[condition];
+
+            if (a > b) {
+                return 1;
             }
+            if (b > a) {
+                return -1;
+            }
+            return 0;
+            
+            // if (condition === 'src'){
+            //     if (a[condition] > b[condition]) {
+            //         return 1;
+            //     }
+            //     if (a[condition] < b[condition]) {
+            //         return -1;
+            //     }
+            //     return 0;
+            // } else {
+            //     if (a.attributes[condition] > b.attributes[condition]) {
+            //         return 1;
+            //     }
+            //     if (a.attributes[condition] < b.attributes[condition]) {
+            //         return -1;
+            //     }
+            //     return 0;
+            // }
+        });
+        this.addToHtml();
+    }
+    
+    function getAttributes(attr) {
+        return Object.fromEntries(attr.split(',').map(function (item) {
+            return item.split(':');
+        }));
+    }
+    
+    function error(param) {
+        if (!param){
+            alert("вы не то делаете");
+            return false;
         }
+    }
+    
+    function createImg(item, galleryWrapper) {
+        var img = document.createElement('img');
+    
+        /*img.addEventListener('click', function () {
+            if (confirm('А вы уверенны?')) {
+                removeItem(this.getAttribute('src'));
+                this.remove();
+            }
+        });*/
+    
+        img.setAttribute('src', item.src);
+        
+        for (var key in item.attributes) {
+            img.setAttribute(key, item.attributes[key])
+        }
+    
+        addImg(img, galleryWrapper);
+    }
+    
+    function addImg(img, galleryWrapper) {
+        galleryWrapper.append(img);
+    }
+    
+    return {
+        addCollection: function (item, callback) {
+            if (error(item)) return false;
+            
+            item = item.split('|');
+    
+            var src = item[0],
+                attributes = getAttributes(item[1]);
+            
+            collection.push({
+                src: src,
+                attributes: attributes,
+            });
+            
+            if (callback && typeof callback === "function") callback();
+        },
+        getCollection: function () {
+            return collection;
+        },
+        removeCollection: function (src) {
+            if (error(src)) return false;
+            collection = collection.filter(function (item) {
+                return item.src !== src;
+            });
+            removeHtml(src, galleryWrapper);
+        },
+        addToHtml: function () {
+            galleryWrapper.innerHTML = '';
+            collection.forEach(function (item) {
+                createImg(item, galleryWrapper);
+            });
+        },
+        sortCollection: function (condition) {
+            sortCollection.call(this, condition, galleryWrapper);
+        }
+    }
 }
-var slider1 = slider();
-console.log(slider1.addCollection('ima/slider/01.jpg|alt:slide1,data-text:my slide'))
+
+var slider1 = slider('gallery1');
+var addBtn = document.getElementById('add'),
+    removeBtn = document.querySelector('#remove'),
+    sortBtn = document.querySelector('#sort');
+
+addBtn.addEventListener('click', function () {
+    slider1.addCollection(
+        prompt('Enter the item:', 'img1.jpg|alt:dsfgdshjf,title:my image'),
+        function () {
+            slider1.addToHtml();  
+            // document.getElementById('gallery1')
+        }
+    );
+    
+    console.log(slider1.getCollection());
+});
+
+removeBtn.addEventListener('click', function () {
+    slider1.removeCollection(
+        prompt('Enter the removed src:', '../images/slider/01.jpg')
+        // document.getElementById('gallery1')
+        );
+    console.log(slider1.getCollection());
+});
+
+sortBtn.addEventListener('click', function () {
+    slider1.sortCollection('src');
+    // document.getElementById('gallery1')
+});
