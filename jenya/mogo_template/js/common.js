dd2dcd4f4bed2a8fe73647b6ee7be6db381260b5
Evgeniy Kamenchuk <jeconda@Evgeniys-MacBook-Pro.local>
2020-06-11 00:52:57 +0300

    service section

--- jenya/mogo_template/js/common.js
+++ jenya/mogo_template/js/common.js
@@ -1,19 +1,10 @@
-// var card = document.getElementById('active_click');
-// card.addEventListener('click', function () {
-//     card.classList.add('active');
-// });
 var cards = document.getElementsByClassName('about_item');
-// console.log(cards);
-for (var i = 0; i < cards.length; i++) {
-    cards[i].addEventListener('click', function () {
-        var card = cards[0];
-        while (card) {
-            if (card.tagName === "DIV") {
-                card.classList.remove("active");
-            }
-            card = card.nextSibling;
+for(var i = 0; i < cards.length; i++){
+    cards[i].addEventListener('click', function(){
+        var active = document.getElementsByClassName("active");
+        if(active.length > 0){
+            active[0].className = active[0].className.replace(" active", "");
         }
-        this.classList.add("active");
-    })
+        this.className += " active"
+    });
 }
-// https://www.youtube.com/watch?v=RBB2N341tr0
