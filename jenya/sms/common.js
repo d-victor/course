26c7c4afc76dd60981b608783886903ca79010ee
Evgeniy Kamenchuk <jeconda@Evgeniys-MacBook-Pro.local>
2020-07-11 01:15:41 +0300

    percent rate counter

--- jenya/sms/common.js
+++ jenya/sms/common.js
@@ -115,11 +115,11 @@
             this.options = {
                 wrapper: options.wrapper,
                 textarea: options.wrapper.querySelector('textarea'),
-                addMsgBtn:options.wrapper.querySelector('#addMsg'),
-                formText: options.wrapper.querySelector('.form-text'),
-                formMessages: options.wrapper.querySelector('.form-messages'),
                 userName: options.wrapper.querySelector('#userName'),
                 userEmail: options.wrapper.querySelector('#userEmail'),
+                addMsgBtn: options.wrapper.querySelector('#addMsg'),
+                formText: options.wrapper.querySelector('.form-text'),
+                formMessages: options.wrapper.querySelector('.form-messages'),
                 icons: {
                     wrapper: options.wrapper.querySelectorAll('.form-icon li'),
                     iconLists: {}
@@ -128,7 +128,6 @@
                 statusUpdate: false,
                 updateId: null
             };
-
             var search = window.location.search;
             
             this.options.textarea.addEventListener('input', inputText.bind(this));
@@ -143,77 +142,81 @@
             }, this);
         
             this.options.addMsgBtn.addEventListener('click', function () {
-                if (!this.options.statusUpdate){
+                if (!this.options.statusUpdate) {
                     addMessage.call(this, setText.call(this, this.options.textarea.value));
+                    
                 } else {
-                    var messages = getJsonMessages('chat');
-                    messages = messages.map(function(message){
-                        if(this.options.updateId === +message.id){
+                    var messages = JSON.parse(getLocalStorage('chat'));
+                    messages = messages.map(function (message) {
+                        if (this.options.updateId === +message.id) {
                             message.user.name = this.options.userName.value;
                             message.user.email = this.options.userEmail.value;
                             message.message = setText.call(this, this.options.textarea.value);
                         }
                         return message;
                     }.bind(this));
+                    
                     saveLocalStorage('chat', messages);
+                    
                     cleanForm.call(this);
+                    
                     this.options.formMessages.innerHTML = '';
-
-                    // -----------
-                    messageReverse.call(this, messages);
-                    // ------------
-                    // messages.reverse().forEach(function(message){
-                    //     console.log(message);
-                    //     addMessage.call(this, message);
-                    // }, this);
+                    
+                    messages.reverse().forEach(function (message) {
+                        addMessage.call(this, message);
+                    }, this);
                     this.options.updateId = null;
                     this.options.statusUpdate = false;
                     this.options.addMsgBtn.textContent = 'Add message';
                 }
             }.bind(this));
-
-            window.addEventListener('storage', function (e){
-                console.log(JSON.parse(e.oldValue));
-                addMessage.call(this, JSON.parse(e.newValue)[0]);
+        
+            window.addEventListener('storage', function (e) {
+                var newValue = JSON.parse(e.newValue),
+                    oldValue = JSON.parse(e.oldValue);
+                
+                console.log(newValue, oldValue);
+                if (newValue.length > oldValue.length) {
+                    addMessage.call(this, JSON.parse(e.newValue)[0]);
+                    
+                } else if (newValue.length < oldValue.length) {
+                    var message = oldValue.find(function (oldMessageValue) {
+                        var messageListId = newValue.map(function (newMessageValue) {
+                            return newMessageValue.id;
+                        });
+                        
+                        return messageListId.indexOf(oldMessageValue.id) === -1;
+                    });
+                    
+                    removeMessageToHtml.call(this, message.id);
+                } else {
+                    this.options.formMessages.innerHTML = '';
+                    setLoadingMessages.call(this);
+                }
             }.bind(this));
-
-            if(search){
+            
+            if (search) {
                 search = search.slice(1).split('&');
-
-                search.forEach(function(res){
+                
+                search.forEach(function (res) {
                     var inputArr = res.split('=');
-                    inputArr[1] = decodeURI(inputArr[1])
-                    if(inputArr[0] === 'name') this.options.userName.value = inputArr[1];
-                    if(inputArr[0] === 'email') this.options.userEmail.value = inputArr[1];
-                    if(inputArr[0] === 'm') {
+                    inputArr[1] = decodeURI(inputArr[1]);
+                    if (inputArr[0] === 'name') this.options.userName.value = inputArr[1];
+                    if (inputArr[0] === 'email') this.options.userEmail.value = inputArr[1];
+                    if (inputArr[0] === 'm') {
                         this.options.textarea.value = inputArr[1];
                         this.options.textarea.dispatchEvent(inputEvent);
                     }
                 }.bind(this));
-
             }
-
-            // console.log(window.location.search);
-
-            // --------------------
-            function messageReverse(messageInfo){
-                messageInfo.reverse().forEach(function(message){
-                    console.log(message);
-                    addMessage.call(this, message);
-                }, this);
-            }
-            // ----------------------
             
             function setLoadingMessages() {
-                var dataMessages = getJsonMessages('chat');
-                console.log(dataMessages);
-                if(dataMessages && dataMessages.length > 0){
-                    messageReverse.call(this, dataMessages);
-                    // dataMessages.reverse().forEach(function (message) {
-                    //     addMessage.call(this, message);
-                    // }, this);
+                var dataMessages = JSON.parse(getLocalStorage('chat'));
+                if (dataMessages && dataMessages.length > 0) {
+                    dataMessages.reverse().forEach(function (message) {
+                        addMessage.call(this, message);
+                    }, this);
                 }
-
             }
             setLoadingMessages.call(this);
             
@@ -232,53 +235,68 @@
                 el.selectionStart = el.selectionEnd = endIndex + text.length + offset;
                 el.focus();
             }
-
-            function deleteMessages(e){
+            
+            function removeMessageToHtml(messageId) {
+                this.options.formMessages.querySelector('[data-id="' + messageId + '"]').remove();
+            }
+        
+            function deleteMessages(e) {
                 var idMessage = e.currentTarget.dataset.btnId;
-                console.log(this.options.formMessages.querySelector('[data-id="' + idMessage + '"]'));
-                this.options.formMessages.querySelector('[data-id="' + idMessage + '"]').remove();
+                removeMessageToHtml.call(this, idMessage);
+            
                 deleteLocalStorage(idMessage);
-                --this.options.countMessages;
             }
-
-            function deleteLocalStorage(id){
-                var dataChat = getJsonMessages('chat');
-                // console.log(dataChat);
-                dataChat = dataChat.filter(function(message){
-                    return message.id !== +id;
+            
+            function deleteLocalStorage(id) {
+               var dataChat = JSON.parse(getLocalStorage('chat'));
+               
+                dataChat = dataChat.filter(function (message) {
+                   return message.id !== +id;
                 });
-
-                saveLocalStorage('chat' ,dataChat);
-                // console.log(dataChat);
-
+        
+                saveLocalStorage('chat', dataChat);
             }
-
-            function editTextForMessage(text){
+            
+            function editTextForMessage(text) {
                 var iconList = this.options.icons.iconLists;
-
-                for (var key in iconList){
-                    text = text.replace(new RegExp('<i class="'+iconList[key]+ '"></i>', 'g'), '::' + key + '::');
+                
+                for (var key in iconList) {
+                    text = text.replace(new RegExp('<i class="' + iconList[key] + '"></i>', 'g'), '::' + key + '::');
                 }
-
+                
                 return text;
             }
-
-            function editMessage(e){
+            
+            function editMessage(e) {
                 var idMessage = +e.currentTarget.dataset.btnId;
-                // console.log(idMessage);
-                var messages = getJsonMessages('chat');
-                var message = messages.find(function (messageObj){
+                console.log(idMessage);
+                var messages = JSON.parse(getLocalStorage('chat'));
+                var message = messages.find(function (messageObj) {
                     return +messageObj.id === idMessage;
                 });
+                
                 this.options.userName.value = message.user.name;
                 this.options.userEmail.value = message.user.email;
                 this.options.textarea.value = editTextForMessage.call(this, message.message);
                 this.options.textarea.dispatchEvent(inputEvent);
                 this.options.statusUpdate = true;
                 this.options.updateId = idMessage;
-                this.options.addMsgBtn.textContent = 'Aply message';
-
-                // console.log(message);
+                this.options.addMsgBtn.textContent = 'Apply';
+            }
+            
+            function rating(e) {
+                var messageId = e.currentTarget.dataset.btnId,
+                    direction = Boolean(+e.currentTarget.dataset.rate),
+                    messageList = JSON.parse(getLocalStorage('chat')),
+                    message = messageList.find(function (findMessage) {
+                        return +findMessage.id === +messageId;
+                    });
+                
+                    message.rating = direction ? ++message.rating : --message.rating;
+                    
+                    saveLocalStorage('chat', messageList);
+                    
+                this.options.formMessages.querySelector('[data-id="' + messageId + '"] .footer .rating').textContent = message.rating;
             }
             
             function addMessage(message) {
@@ -286,15 +304,13 @@
                     headerWrap = document.createElement('div'),
                     contentWrap = document.createElement('div'),
                     footerWrap = document.createElement('div'),
-                    // user = this.options.userName.value,
-                    // email = this.options.userEmail.value,
                     btnDelete = document.createElement('button'),
                     btnEdit = document.createElement('button'),
-                    year, time, userName, userEmail, idMessage;
-
-                    // var idMessage = ++this.options.countMessages;
-
-                    
+                    btnPlus = document.createElement("button"),
+                    btnMinus = document.createElement("button"),
+                    year, time, userName, userEmail, idMessage,
+                    messageRating = 0;
+                
                 if (typeof message === 'string') {
                     idMessage = ++this.options.countMessages;
                     year = getDate().year;
@@ -312,83 +328,98 @@
                         date: {
                             year: year,
                             time: time
-                        }
+                        },
+                        rating: messageRating
                     });
                 } else {
                     idMessage = message.id;
-                    if(this.options.countMessages < idMessage) {
+                    
+                    if (this.options.countMessages < idMessage) {
                         this.options.countMessages = idMessage;
                     }
+                    
                     year = message.date.year;
                     time = message.date.time;
                     userName = message.user.name;
                     userEmail = message.user.email;
+                    messageRating = message.rating
                 }
-
+                
                 btnDelete.classList.add('delete-btn');
-                btnDelete.addEventListener('click' , deleteMessages.bind(this));
-                btnDelete.textContent = 'delete message';
-                btnDelete.dataset.btnId = typeof message === 'string' ? idMessage : message.id;
-
+                btnDelete.addEventListener('click', deleteMessages.bind(this));
+                btnDelete.textContent = 'Delete the message';
+                btnDelete.dataset.btnId = idMessage;
+                
                 btnEdit.classList.add('edit');
                 btnEdit.addEventListener('click', editMessage.bind(this));
                 btnEdit.dataset.btnId = idMessage;
                 btnEdit.textContent = "Edit";
-
-
-                btnDelete.dataset.btnId = idMessage;
-                headerWrap.classList.add('header_item');
-                contentWrap.classList.add('content_item');
-                footerWrap.classList.add('footer_item');
-                footerWrap.textContent = year + 'year Time: ' + time;
-                headerWrap.textContent = 'username: ' + userName + ' ' + ' email: ' + userEmail;
+                
+                btnPlus.addEventListener('click', rating.bind(this));
+                btnPlus.dataset.rate = '1';
+                btnPlus.dataset.btnId = idMessage;
+                btnPlus.textContent = 'Like';
+                
+                btnMinus.addEventListener('click', rating.bind(this));
+                btnMinus.dataset.rate = '0';
+                btnMinus.dataset.btnId = idMessage;
+                btnMinus.textContent = 'Dislike';
+                
+                headerWrap.classList.add('header');
+                contentWrap.classList.add('content');
+                footerWrap.classList.add('footer');
+                headerWrap.textContent = year + 'г. Время: ' + time;
+                headerWrap.textContent += " | Name: " + userName + " | Email: " + userEmail + ".";
         
                 newMessageWrapper.classList.add('item-message');
                 newMessageWrapper.dataset.id = idMessage;
-        
+                
                 contentWrap.innerHTML = typeof message === 'string' ? message : message.message;
                 
                 newMessageWrapper.append(headerWrap);
                 newMessageWrapper.append(contentWrap);
+                
                 footerWrap.append(btnDelete);
                 footerWrap.append(btnEdit);
+                footerWrap.append(btnPlus);
+                footerWrap.append(btnMinus);
+                
+                var span = document.createElement('span');
+                span.textContent = messageRating;
+                span.classList.add('rating');
+                footerWrap.append(span);
+                
                 newMessageWrapper.append(footerWrap);
         
-                this.options.formMessages.append(newMessageWrapper);
+                this.options.formMessages.prepend(newMessageWrapper);
             }
             
             function cleanForm() {
                 this.options.textarea.value = '';
                 this.options.formText.innerHTML = '';
-                this.options.userName.value = '';
                 this.options.userEmail.value = '';
+                this.options.userName.value = '';
             }
             
-            function getJsonMessages(key){
-                // console.log(key);
-                return JSON.parse(getLocalStorage(key));
-            }
-
             function setLocalStorage(dataSaved) {
                 var key = 'chat',
-                    // dataChat = JSON.parse(getLocalStorage(key)),
-                    dataChat = getJsonMessages(key),
+                    dataChat = JSON.parse(getLocalStorage(key)),
                     addArray = [];
-
-                    addArray.push(dataSaved);    
                 
-                if (dataChat){
+                addArray.push(dataSaved);
+                
+                if (dataChat) {
                     dataChat.forEach(function (objMessage) {
                         addArray.push(objMessage);
                     })
                 }
                 
                 saveLocalStorage(key, addArray);
-                
             }
-
-            function saveLocalStorage(key, addArray){
-                if(typeof key !== 'string' && addArray && addArray.length != 0) return false;
+            
+            function saveLocalStorage(key, addArray) {
+                if(typeof key !== 'string' && typeof addArray === 'object' && addArray.length !== 0) return false;
+                
                 localStorage.setItem(key, JSON.stringify(addArray));
             }
             
@@ -425,22 +456,9 @@
                 
                 return {
                     year: date.toLocaleString('ru-RU', optionsYear),
-                    time: date.toLocaleString('ru-RU', optionsTime)
+                    time: date.toLocaleString('ru-RU', optionsYear)
                 }
             }
-// -------------
-            // function getInputData(){
-            //     optionsInput = {
-            //         user: options.wrapper.querySelector('#userName').value,
-            //         email: options.wrapper.querySelector('#userEmail').value
-            //     };
-            //     // console.log(optionsInput);
-            //     return {
-            //         user: optionsInput.toLocaleString('ru-RU'),
-            //         email: optionsInput.toLocaleString('ru-RU')
-            //     }
-            // }
-// -------------------
             
             function setText(text) {
                 var list = this.options.icons.iconLists;
